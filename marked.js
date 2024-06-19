def3a68c7789122b762b943414ece3b0059af4da
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2024-06-19 00:21:52 -0500

    Create marked.js

---
+++ marked.js
@@ -0,0 +1,31 @@
+(function (global) {
+  'use strict';
+
+  function escapeHtml(html) {
+    return html.replace(/&/g, '&amp;')
+               .replace(/</g, '&lt;')
+               .replace(/>/g, '&gt;')
+               .replace(/"/g, '&quot;')
+               .replace(/'/g, '&#39;');
+  }
+
+  function parseMarkdown(markdown) {
+    var html = markdown
+      .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+      .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+      .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+      .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+      .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+      .replace(/\*(.*)\*/gim, '<i>$1</i>')
+      .replace(/!\[(.*?)\]\((.*?)\)/gim, '<img alt="$1" src="$2" />')
+      .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2">$1</a>')
+      .replace(/\n$/gim, '<br />');
+
+    return html.trim();
+  }
+
+  global.marked = function (markdown) {
+    return parseMarkdown(escapeHtml(markdown));
+  };
+
+}(this));
